c2191f232450bef1178773a1567727c5e327e920
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-13 19:47:23 -0500

    adding fetch for location show dispatch

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,8 +1,15 @@
 const url = "http://localhost:3000/"
 
 export const getLocations = (zipcode) => {
-  return dispatch => fetch(url + "locations/zip-search" + zipcode)
+  return dispatch => fetch(`${url}locations/zip-search/${zipcode}`)
     .then(response => response.json())
     .then(locations => dispatch({type: "GET_LOCATIONS", payload: locations})
     )
+}
+
+export const getLocation = (id) => {
+  return dispatch => fetch(`${url}locations/${id}`)
+    .then(response => response.json())
+    .then(location => dispatch({type: "GET_LOCATION", payload: location})
+  )
 }
