e5442940391f0982a16426fc0e42f794311203e9
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-13 19:55:30 -0500

    adding signup action fetch for users

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -13,3 +13,20 @@ export const getLocation = (id) => {
     .then(location => dispatch({type: "GET_LOCATION", payload: location})
   )
 }
+
+export const clearLocation = () => ({type: "CLEAR_LOCATION"})
+
+export const submitSignup = (user) => {
+  return dispatch => fetch(`${url}users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(result => result.json())
+  .then(response => {
+    localStorage.token = response.token
+    dispatch({type: "SET_USER", payload: response.user})
+  })
+}
