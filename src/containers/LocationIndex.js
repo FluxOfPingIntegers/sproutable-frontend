6eb9dc10038528dfbd2de7da3580585546165fc3
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-15 10:38:53 -0500

    adding Location Card Links to Index

---
+++ src/containers/LocationIndex.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+import { getLocations } from '../redux/actionCreators'
+import { connect } from 'react-redux'
+import { LocationCard } from "../components/LocationCard"
+
+function LocationIndex({getLocations, locations}){
+  useEffect(getLocations, [getLocations])
+
+  return <div className="cards">
+      {locations.map(location => <LocationCard {...location} key={location.id}/>)}
+  </div>
+}
+
+const mapStateToProps = (state) => {
+  return {locations: state.locations}
+}
+
+export default connect(mapStateToProps, { getLocations })(LocationIndex)
