8572e00a1aad7e9f5fac2245d52976e8f3ce7de6
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-15 11:01:37 -0500

    adding LocationCard and LocationShow

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,14 +1,9 @@
 import { Link } from 'react-router-dom'
 
-function LocationCard({id, name, image, hours, address, zipcode}){
+function LocationCard({id, name, image}){
   return <div className="card">
     <Link to={`/locations/${id}`}><h3>{name}</h3></Link>
     <Link to={`/locations/${id}`}><img src={image} alt={name}/></Link>
-    <ul>
-      <li>Address: {address}</li>
-      <li>Zip Code: {zipcode}</li>
-      <li>Hours: {hours}</li>
-    </ul>
   </div>
 }
 
--- src/components/LocationShow.js
+++ src/components/LocationShow.js
@@ -1,5 +1,33 @@
+import { useParams } from 'react-router-dom'
+import {connect} from 'react-redux'
+import {getLocation, clearLocation} from '../redux/actionCreators'
+import { useEffect } from 'react'
+
 function LocationShow({id, name, image, description, hours, address, zipcode}){
-  return <div className="LocationShow">
-    
+  const locationId = useParams().id
+
+  useEffect(() => {
+    getLocation(locationId)
+    return clearLocation
+  }, [getLocation, locationId, clearLocation])
+
+  const loading = () => <p>...Loading...</p>
+
+  const loadedPage = () => <div className="show">
+    <h1>{name}</h1>
+    <img src={image} alt={name}/>
+    <ul>
+      <li>Address: {address}, Zipcode: {zipcode}</li>
+      <li>Hours: {hours}</li>
+    </ul>
+    <p>{description}</p>
   </div>
+
+  return id ? loadedPage() : loading()
 }
+
+const mapStateToProps = (state) => {
+  return {...state.selectedLocation}
+}
+
+export default connect(mapStateToProps, {getLocation, clearLocation})(LocationShow);
