74e68d9167715271ebfa9fa15fa6303dd990e1c8
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-15 07:35:19 -0500

    working on Auth form

---
+++ src/components/Auth.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { submitSignup, submitLogin } from '../redux/actionCreators'
+import { connect } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+function Auth(props){
+
+  const [signup, setSignup] = useState(false)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [zipCode, setZipCode] = useState(20500)
+  const history = useHistory()
+
+  const toggleSignup = () => setSignup(!signup)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    signup ? props.submitSignup({ username, password, zip_code: zipCode }) : props.submitLogin({username, password})
+    history.pushState("/locations/zip-search/:zipcode")
+  }
+
+  return (
+    <div class="Auth">
+      {signup ? <h1>Sign up!</h1> : <h1>Login!</h1>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        {signup && <label>
+          Zip Code:
+          <input type="number" name="zipCode" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+        </label>}
+        <label>
+          Password:
+          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+        <button onClick={toggleSignup}>Or... {signup ? "Login!" : "Signup!"}</button>
+    </div>
+  )
+}
+
+export default connect(null, { submitSignup, submitLogin })(Auth);
