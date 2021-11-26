2136dee3f249b18be3a365b0c866c1fa56fd57d3
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-26 15:54:00 -0500

    separating initial states for more readable reducer

---
+++ src/redux/initialStates.js
@@ -0,0 +1,30 @@
+export const initialLocation = {
+    location: {
+      usda_id: 0,
+      yelp_id: "",
+      name: "",
+      description: "",
+      address: "",
+      zipcode: 0,
+      hours: "",
+      fee: 0,
+      pass: 0,
+      image: "",
+    },
+    events: []
+  }
+  
+export const initialEvent = Object.assign(initialLocation.location, {
+  date: "",
+  vendors: [],
+  items: []
+})
+
+export const initialUser = {
+  username: "",
+  name: "",
+  email: "",
+  image: "",
+  zipcode: "",
+  venmoname: ""
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,34 +1,4 @@
-
-const initialLocation = {
-  location: {
-    usda_id: 0,
-    yelp_id: "",
-    name: "",
-    description: "",
-    address: "",
-    zipcode: 0,
-    hours: "",
-    fee: 0,
-    pass: 0,
-    image: "",
-  },
-  events: []
-}
-
-const initialEvent = Object.assign(initialLocation.location, {
-  date: "",
-  vendors: [],
-  items: []
-})
-
-const initialUser = {
-  username: "",
-  name: "",
-  email: "",
-  image: "",
-  zipcode: "",
-  venmoname: ""
-}
+import {initialLocation, initialUser, initialEvent} from './initialStates'
 
 const initialState = {
   locations: [],
