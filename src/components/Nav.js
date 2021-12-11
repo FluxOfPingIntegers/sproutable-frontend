f9b79ee24e4ca301059daead620ca6e0dda52d0a
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-12-11 15:41:29 -0500

    redesigning UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,36 @@ import { NavLink } from 'react-router-dom';
 import {connect} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 import {clearUser} from '../redux/actionCreators'
+import ZipSearch from './ZipSearch'
+import { useState } from 'react'
 
 function Nav({user, clearUser}){
+  const [selectedButton, setSelectedButton] = useState("")
+
+  const handleMouseEnter = (e) => {
+    setSelectedButton(e.target.id)
+    e.preventDefault()
+  }
+
+  const handleMouseLeave = (e) => {
+    setSelectedButton("")
+    e.preventDefault()
+  }
+
+  const buttonSize = (elementId) => {
+    if (elementId === selectedButton) {
+      return "large"
+    } else {
+      return "small"
+    }
+  }
 
   const authOptions = (
     <>
-      <NavLink to="/users/login">Login</NavLink> |<> </>
-      <NavLink to="/users/signup">Signup</NavLink> |<> </>
+      <NavLink to="/users/login" onMouseEnter={handleMouseEnter} 
+        onMouseLeave={handleMouseLeave} id="login-button" className={buttonSize("login-button")}>Login</NavLink> |<> </>
+      <NavLink to="/users/signup" onMouseEnter={handleMouseEnter} 
+        onMouseLeave={handleMouseLeave} id="signup-button" className={buttonSize("signup-button")}>Signup</NavLink> |<> </>
     </>
   )
 
@@ -23,15 +46,16 @@ function Nav({user, clearUser}){
   const userDisplay = (
     <>
       Logged in as: <NavLink to="/users/1">{user.username}</NavLink> |<> </>
-      <NavLink to="/" onClick={handleLogoutClick}>Logout</NavLink> | <> </>
+      <NavLink to="/" onClick={handleLogoutClick} id="logout-button" className={buttonSize("lgout-button")}>Logout</NavLink> | <> </>
     </>
   )
 
   return <nav className="Nav">
-    <div id="nav">
+    <>
     {!!user.username ? userDisplay : authOptions}
-    <NavLink to="/locations/zip-search">Search Markets By Zip</NavLink>
-    </ div>
+    <ZipSearch />
+    {/* <NavLink to="/locations/zip-search">Search Markets By Zip</NavLink> */}
+    </>
   </nav>
 }
 
