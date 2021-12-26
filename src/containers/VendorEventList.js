de87e7039ec0b64b624d5e480b51b2067c74a647
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-12-26 08:07:55 -0500

    adding VendorEventList container

--- src/containers/VendorEventList.js
+++ src/containers/VendorEventList.js
@@ -1,13 +1,40 @@
-import {  } from '../redux/actionCreators'
-
+import { getEvents, clearEvents } from '../redux/actionCreators'
+import { useEffect } from "react"
 import { connect } from 'react-redux'
 
-function VendorEventList({vendor}) {
+function VendorEventList({vendor, getEvents, events}) {
+
+  useEffect(() => {
+    if (!!vendor.zipcode) {
+      getEvents({zipcode: vendor.zipcode})
+    }
+    return clearEvents
+  }, [getEvents, clearEvents])
+
+  const eventList = () => {
+    return (
+      <>
+        <h3>The Following Are A List Of Nearby Events</h3>
+        <p>Please check the ones you intend to sell your products at</p>
+        <ul>
+          {events.map(event => <EventSelect event={event} key={event.id} />)}
+        </ul>
+      </>
+    )
+  }
+
+  const zipPrompt = () => {
+    return <p>Please provide your zip code within your vendor profile for a list of nearby events</p>
+  }
+
+  const eventDisplay = (!!vendor.zipcode ? eventList() : zipPrompt() )
+
+  return eventDisplay
 
 }
 
 const mapStateToProps = (state) => {
-  return {locations: state.locations}
+  return {events: state.selectedEvents}
 }
 
-export default connect(mapStateToProps, { getLocations })(VendorEventList)
+export default connect(mapStateToProps, { getEvents })(VendorEventList)
